f5f6d4c4a2712a0a67f7cf1235cf67aff2bb1f6c
Bruno Teixeira <bruno.teixeira@locafox.de>
2015-03-23 19:51:33 +0100

    Initial import. I hope I never have to do another commit.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # supersimplehttpserver
-One-file HTTP server for static files
+One-file HTTP server for static files.
+
+Put it in your directory and start it:
+
+```
+git clone https://github.com/bteixeira/supersimplehttpserver.git
+node supersimplehttpserver/server
+```
+
+Optionally pass in a port number as a single argument
+```
+node supersimplehttpserver/server 9002
+```
+
+Notice that the default port is 9001, which is over nine thousand.  
+  
+That's it. It just serves all files in that directory (and subdirs).  
+  
+No, it doesn't do anything else.  
+Yes, the browser will complain about MIME types.
---
+++ server.js
@@ -0,0 +1,55 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+    /* Log every request */
+    console.log('[' + new Date() + '] ' + req.url);
+
+    var filename = '.' + req.url;
+    try {
+        var stat = fs.statSync(filename);
+        if (stat.isFile()) {
+            /* Send file if it exists */
+            fs.readFile(filename, function (err, data) {
+                if (err) {
+                    console.log('error reading file "' + filename + '": ' + err);
+                    res.writeHead(500);
+                    res.end();
+                } else {
+                    res.end(data);
+                }
+            });
+        } else if (stat.isDirectory()) {
+            /* List dir contents if it exists */
+            fs.readdir(filename, function (err, data) {
+                if (err) {
+                    console.log('error reading dir "' + filename + '": ' + err);
+                    res.writeHead(500);
+                    res.end();
+                } else {
+					res.write('<html><head><title>Listing for ' + filename + '</title></head><body><h1>Listing for <pre>' + filename + '</pre></h1>');
+                    //res.write(data.join('<br>'));
+					data.forEach(function (item) {
+						res.write('<pre><a href="' + req.url + (req.url[req.url.length - 1] === '/' ? '' : '/') + item + '">' + item + '</a></pre>');
+					});
+					res.end('</body></html>');
+                }
+            });
+        } else {
+            console.log('other');
+            res.writeHead(403);
+            res.end();
+        }
+    } catch (e) {
+        console.log('returning 404:', e.message);
+        res.writeHead(404);
+        res.end('File not found! ' + filename);
+    }
+
+});
+
+var port = parseInt(process.argv[2], 10) || 9001;
+
+server.listen(port, function () {
+    console.log('Ready on port', port);
+});
