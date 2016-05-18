55af147f6fe35d2014e5b4d9ca2e9fc0ad39c0ed
Bruno Teixeira <bruno.teixeira@locafox.de>
2016-05-18 18:52:17 +0200

    Brought the server to the new century

--- server.js
+++ server.js
@@ -10,32 +10,31 @@ var server = http.createServer(function (req, res) {
         var stat = fs.statSync(filename);
         if (stat.isFile()) {
             /* Send file if it exists */
-            fs.readFile(filename, function (err, data) {
-                if (err) {
-                    console.log('error reading file "' + filename + '": ' + err);
-                    res.writeHead(500);
-                    res.end();
-                } else {
-                    res.end(data);
-                }
-            });
+            fs.createReadStream(filename).pipe(res);
         } else if (stat.isDirectory()) {
             /* List dir contents if it exists */
             fs.readdir(filename, function (err, data) {
                 if (err) {
-                    console.log('error reading dir "' + filename + '": ' + err);
+                    console.error('error reading dir "' + filename + '": ' + err);
                     res.writeHead(500);
                     res.end();
                 } else {
-                    res.write('<html><head><title>Listing for ' + filename + '</title></head><body><h1>Listing for <pre>' + filename + '</pre></h1>');
-                    data.forEach(function (item) {
-                        res.write('<pre><a href="' + req.url + (req.url[req.url.length - 1] === '/' ? '' : '/') + item + '">' + item + '</a></pre>');
-                    });
-                    res.end('</body></html>');
+                    var parent = req.url + (req.url[req.url.length - 1] === '/' ? '' : '/');
+                    res.write(`
+                        <html>
+                        <head><title>Listing for ${filename}</title></head>
+                        <body>
+                            <h1>Listing for <pre>${filename}</pre></h1>
+                            <ul>
+                                ${data.map(item => `<li><pre><a href="${parent + item}">${item}</a></pre></li>`).join('\n')}
+                            </ul>
+                        </body>
+                        </html>
+                    `);
                 }
             });
         } else {
-            console.log('other');
+            console.error('Path is neither file nor directory, I don\'t know what to do');
             res.writeHead(403);
             res.end();
         }
