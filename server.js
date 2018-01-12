92209ce69d38265c92c7d43b54e9e99c16e76368
Bruno Teixeira <bteixeira666@gmail.com>
2018-01-12 12:10:10 +0100

    Improved code style: - Indentation (yes, I use tabs) - Replaced inline functions with arrows - Replaced vars with consts - Moved the port parsing up

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 
-var server = http.createServer(function (req, res) {
-    /* Log every request */
-    console.log(`[${new Date()}] [${req.socket.address().address}] ${req.url}`);
+/* Listen on specified port or use the default 9001 */
+const port = parseInt(process.argv[2], 10) || 9001;
 
-    var filename = '.' + decodeURI(req.url);
-    fs.stat(filename, function(err, stat) {
-        if (err) {
-            console.log(`Returning 404: ${err.message}`);
-            res.writeHead(404);
-            res.end(`File not found: "${filename}"`);
-        } else if (stat.isFile()) {
-            /* Send file if it exists */
-            fs.createReadStream(filename).pipe(res);
-        } else if (stat.isDirectory()) {
-            /* List dir contents if it exists */
-            fs.readdir(filename, function (err, data) {
-                if (err) {
-                    console.error(`Error reading dir "${filename}": ${err}`);
-                    res.writeHead(500);
-                    res.end();
-                } else {
-                    var parent = req.url + (req.url[req.url.length - 1] === '/' ? '' : '/');
-                    res.end(`
+http.createServer((req, res) => {
+	/* Log every request */
+	console.log(`[${new Date()}] [${req.socket.address().address}] ${req.url}`);
+
+	const filename = '.' + decodeURI(req.url);
+	fs.stat(filename, (err, stat) => {
+		if (err) {
+			console.log(`Returning 404: ${err.message}`);
+			res.writeHead(404);
+			res.end(`File not found: "${filename}"`);
+		} else if (stat.isFile()) {
+			/* Send file if it exists */
+			fs.createReadStream(filename).pipe(res);
+		} else if (stat.isDirectory()) {
+			/* List dir contents if it exists */
+			fs.readdir(filename, (err, data) => {
+				if (err) {
+					console.error(`Error reading dir "${filename}": ${err}`);
+					res.writeHead(500);
+					res.end();
+				} else {
+					const parent = req.url + (req.url[req.url.length - 1] === '/' ? '' : '/');
+					res.end(`
                         <html>
                         <head><title>Listing for ${filename}</title></head>
                         <body>
@@ -34,18 +37,12 @@ var server = http.createServer(function (req, res) {
                         </body>
                         </html>
                     `);
-                }
-            });
-        } else {
-            console.error(`Path "${filename}" is neither file nor directory, I don't know what to do`);
-            res.writeHead(403);
-            res.end();
-        }
-    });
-});
-
-var port = parseInt(process.argv[2], 10) || 9001;
-
-server.listen(port, function () {
-    console.log('Ready on port', port);
-});
+				}
+			});
+		} else {
+			console.error(`Path "${filename}" is neither file nor directory, I don't know what to do`);
+			res.writeHead(403);
+			res.end();
+		}
+	});
+}).listen(port, () => console.log(`Ready on port ${port}`));
